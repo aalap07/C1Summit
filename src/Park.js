2390f3de93f4333564f6d545b987016e99140669
Aalap Patel <aalappatel07@gmail.com>
2019-05-29 00:52:53 -0400

    Adding support for alerts

--- src/Park.js
+++ src/Park.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import style from './park.module.css';
+import Panel from './components/panel/ExpansionPanel';
 
-const Park = ({title, location, parkCode}) => {
+const API_KEY = 'StlUEfYJI8sIZBZPFGZSwb6boSG7aEbXUY9q4lsy';
+const alerts = null;
+
+const Park = ({ title, location, parkCode }) => {
+    const getData = async () => {
+        const alertResponse = await fetch(`https://developer.nps.gov/api/v1/alerts?parkCode=${parkCode}&api_key=${API_KEY}`);
+        const alertData = await alertResponse.json();
+        console.log(alertData.data);
+      }
+
+      useEffect(() => {
+        getData();
+      }, [])
+    
     return (
         <div className={style.park}>
             <h2>{title}</h2>
-            <p>{location}</p>
-            <p> Parkcode: {parkCode} </p>
+            <p>{location} <br></br> Parkcode: {parkCode} </p>
 
-            <button onClick ={(e) => {
-                handleClick(e, parkCode)}} href="#">More info</button>
-        </div> 
+            <button onClick={(e) => {
+                handleClick(e, parkCode)
+            }} href="#">More info</button>
+            <p></p>
+            <Panel
+                parkCode={parkCode}
+               
+            //Alerts, articles, events, news releases
+            />
+        </div>
     );
 }
 
@@ -18,7 +38,7 @@ function handleClick(e, parkCode) {
     e.preventDefault();
     var win = window.open(`https://www.nps.gov/${parkCode}/index.htm`, '_blank');
     win.focus();
-   
-  }
+
+}
 
 export default Park;
