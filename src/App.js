07aed6f29e4893756667221cc24b337b52b754a0
Aalap Patel <aalappatel07@gmail.com>
2019-05-28 23:35:04 -0400

    Setting null value for search initial state

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   const API_KEY = 'StlUEfYJI8sIZBZPFGZSwb6boSG7aEbXUY9q4lsy';
   const [parks, setParks] = useState([]);
   const [search, setSearch] = useState('');
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState('NULL');
 
 
   useEffect(() => {
@@ -17,7 +17,6 @@ function App() {
   }, [query])
 
   const getData = async () => {
-
     const response = await fetch(`https://developer.nps.gov/api/v1/parks?limit=10&q=${query}&api_key=${API_KEY}`);
     const data = await response.json();
     setParks(data.data);
@@ -38,26 +37,26 @@ function App() {
 
     <div className="App">
 
-    <Header/>
-     
-              <h1>Welcome to the National Park Service Kiosk </h1>
+      <Header />
 
-            <form onSubmit={getSearch} className="search-form">
-              <input className="search-bar" type="text" value={search} onChange={updateSearch} />
-              <button className="search-button" type="submit">Search</button>
-            </form>
-            <div className="parks">
-            {parks.map(park => (
-              <Park
-                title={park.fullName}
-                location={park.states}
-                parkCode={park.parkCode}
-               
-              />
-            ))}
-</div>
-          </div>
-          );
-        }
-        
-        export default App;
+      <h1>Welcome to the National Park Service Kiosk </h1>
+
+      <form onSubmit={getSearch} className="search-form">
+        <input className="search-bar" type="text" placeholder="Enter a park..." value={search} onChange={updateSearch} />
+        <button className="search-button" type="submit">Search</button>
+      </form>
+      <div className="parks">
+        {parks.map(park => (
+          <Park
+            title={park.fullName}
+            location={park.states}
+            parkCode={park.parkCode}
+
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
