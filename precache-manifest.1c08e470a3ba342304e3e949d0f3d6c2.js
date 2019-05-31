dd64ee57829e2b05f08cf39a4cdf5d1a3382c87a
Aalap Patel <aalappatel07@gmail.com>
2019-05-31 00:45:48 -0400

    Updates

---
+++ precache-manifest.1c08e470a3ba342304e3e949d0f3d6c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc51a9494b6a92bda075590fbd55434a",
+    "url": "/C1Summit/index.html"
+  },
+  {
+    "revision": "7fe789fff53d39d81dd0",
+    "url": "/C1Summit/static/css/main.68bee5c9.chunk.css"
+  },
+  {
+    "revision": "fdf5f56bcbe560ec622e",
+    "url": "/C1Summit/static/js/2.96e9014b.chunk.js"
+  },
+  {
+    "revision": "7fe789fff53d39d81dd0",
+    "url": "/C1Summit/static/js/main.62f20a9f.chunk.js"
+  },
+  {
+    "revision": "4201187cf4a31678ee23",
+    "url": "/C1Summit/static/js/runtime~main.442ff9e2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/C1Summit/static/media/logo.5d5d9eef.svg"
+  }
+]);
