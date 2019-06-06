ea049b15b33363bac11b2ea3c2afb573fec184c4
Aalap Patel <aalappatel07@gmail.com>
2019-06-06 19:24:41 -0400

    Notifying user if no results

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import Park from './components/park/Park';
 import './App.css';
-import Panel from './components/panel/ExpansionPanel';
 import Header from './components/header/Header'
 
 function App() {
-
-  const API_KEY = 'Osq7v44aBTCLZLAAifD9ybktEZlcIdX0Dw7EfqJg';
   const [parks, setParks] = useState([]);
   const [search, setSearch] = useState('');
   const [query, setQuery] = useState('NULL');
@@ -17,29 +14,36 @@ function App() {
   }, [query])
 
 
+  var count = -1;
   const getData = async () => {
     var array = [];
-    {
-      data.data.map(curr => (
-        curr.fullName.toUpperCase().includes(query.toUpperCase()) ? array.push(curr) : array = array
-      ))
+
+    data.data.map(curr => (
+      curr.fullName.toUpperCase().includes(query.toUpperCase()) ? array.push(curr) : array = array
+    ))
+    count = array.length;
+    if (count == 0 && query !== "NULL") {
+      window.alert("There are no results for " + query  + ".");
+      setSearch('');
     }
-    setParks(array);
+    else {
+      setParks(array);
+    }
   }
 
   const updateSearch = e => {
     const toSet = e.target.value;
     setSearch(toSet);
-    
+
   }
 
   const getSearch = e => {
     e.preventDefault();
     if (search.length < 3) {
-      if (search.length === 0){
+      if (search.length === 0) {
         window.alert("Please enter a valid search.");
       }
-      else{
+      else {
         window.alert("Your search must be at least 3 characters.");
       }
       setSearch("");
@@ -47,7 +51,9 @@ function App() {
     else {
       setQuery(search);
       setParks([]);
+
     }
+
 
   }
 
@@ -61,11 +67,10 @@ function App() {
 
       <form onSubmit={getSearch} className="search-form">
         <input className="search-bar" type="text" placeholder="Enter a park name (3+ characters)" value={search} onChange={updateSearch} />
-       <button className="search-button" type="submit">Search</button>
+        <button className="search-button" type="submit">Search</button>
       </form>
 
       <div className="parks">
-
         {parks.map(park => (
           <Park
             title={park.fullName}
@@ -75,11 +80,14 @@ function App() {
             images={park.images}
             latLong={park.latLong}
           />
-
         ))}
+
       </div>
     </div>
   );
+
 }
+
+
 
 export default App;
